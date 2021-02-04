6d09b28b61d0784b454e5c25459f0e8f01f411a6
Sanaz <sanaz.azh@gmail.com>
2021-02-04 07:57:46 -0500

    create s3 bucket and dynamoDB with CfnOutput to get the name of those resources

--- lib/serverless-application-stack.ts
+++ lib/serverless-application-stack.ts
@@ -1,9 +1,30 @@
-import * as cdk from '@aws-cdk/core';
+import * as cdk from "@aws-cdk/core";
+import * as s3 from "@aws-cdk/aws-s3";
+import * as lambda from "@aws-cdk/aws-lambda";
+import * as dynamodb from "@aws-cdk/aws-dynamodb";
+import * as iam from '@aws-cdk/aws-iam';
+import * as event_sources from '@aws-cdk/aws-lambda-event-sources';
+import { CfnOutput, Duration } from "@aws-cdk/core";
+
+const imageBucketName = 'cdk-rek-imagebucket';
 
 export class ServerlessApplicationStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
+    /**
+     * Image Bucket
+     */
+    const imageBucket = new s3.Bucket(this, imageBucketName);
+    new cdk.CfnOutput(this, 'imageBucket', {value: imageBucket.bucketName});
+    /**
+     * DynamoDB table for storing image labels
+     */
+    const table = new dynamodb.Table(this, 'ImageLabeles', {
+      partitionKey: {name: 'image', type: dynamodb.AttributeType.STRING}
+    });
+    new cdk.CfnOutput(this, 'ddbTable', {value: table.tableName});
+   
+    
   }
 }
+
